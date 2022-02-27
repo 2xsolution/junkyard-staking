b136dfc57d1b7c8b38f9864a1bab7a0b89ce87f5
Prince Bansal <1726250.cse.coe@cgc.edu.in>
2022-02-27 16:35:43 +0530

    Few changes

--- src/Components/Navbar/Navbars.tsx
+++ src/Components/Navbar/Navbars.tsx
@@ -1,41 +1,46 @@
-import React from 'react'
-import { Container, Nav, Navbar } from 'react-bootstrap'
-import { WalletDisconnectButton, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-const pic = '../Image/Screenshot_1-removebg-preview (1) 1.png'
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import {
+  WalletDisconnectButton,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import Logo from "../../assets/Logo.png";
+const pic = "../Image/Screenshot_1-removebg-preview (1) 1.png";
 const Navbars = () => {
   return (
-    <div className=' navbars'>
-      
-        <Navbar collapseOnSelect expand="md" variant="dark">
+    <div className=" navbars">
+      <Navbar
+        collapseOnSelect
+        expand="md"
+        variant="dark"
+        style={{ background: "#030C2A" }}
+      >
         <Container>
           <Navbar.Brand href="#home">
-            <img src={pic} />
+            <img src={Logo} className="logo" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto menu">
+            {/* <Nav className="me-auto menu">
 
               <li><a href="#">Home</a></li>
               <li><a href="#">How to Play</a></li>
               <li><a href="#">Action</a></li>
 
-            </Nav>
-            <Nav>
+            </Nav> */}
+            <Nav className="ms-auto menu">
               <div className="DA_4K">
                 <WalletMultiButton />
               </div>
-
             </Nav>
           </Navbar.Collapse>
-          </Container>
-        </Navbar>
-    
+        </Container>
+      </Navbar>
     </div>
-  )
-}
+  );
+};
 
 export default Navbars;
-
 
 // <div className="nav">
 // <div className="logo">
